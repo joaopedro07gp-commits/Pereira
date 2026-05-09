494ff510d40ffb5d3923a17ae56a624a35e0fc00
João Pedro <joaopedro07gp@gmail.com>
2026-05-09 15:33:51 -0300

    refactor: usar variáveis de ambiente para chaves do Firebase

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,12 +3,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBoBKlwfJy3osa0G-tC9lnv2HsqRo7mgm0",
-  authDomain: "encurtador-links-senai-jp.firebaseapp.com",
-  projectId: "encurtador-links-senai-jp",
-  storageBucket: "encurtador-links-senai-jp.firebasestorage.app",
-  messagingSenderId: "308881822159",
-  appId: "1:308881822159:web:e27f715da4f46f7c02c2ba"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
